19e9db12479bbc0bee26537e0b6825bff5ec2c59
Victor Lee <myleevictor@gmail.com>
2018-02-20 13:31:33 +0800

    Refined closure to be composed within another function

--- Contents.swift
+++ Contents.swift
@@ -1,5 +1,7 @@
 //: A UIKit based Playground for presenting user interface
-  
+// Inspired by LetsBuildThatApp channel
+
+
 import UIKit
 
 func filterLessThanValue(value: Int, numbers: [Int]) -> [Int] {
@@ -19,14 +21,29 @@ func filterLessThanValue(value: Int, numbers: [Int]) -> [Int] {
 func filterWithClosure(closure: (Int) -> Bool, numbers:[Int]) -> [Int] {
 var filteredNumbers = [Int]()
 for num in numbers {
-        if closure(num) {
+        if closure(num) { // if the closure using an Int returns a Bool value
             filteredNumbers.append(num)
     }
 }
 return filteredNumbers
 }
 
-let filteredList = filterWithClosure(closure: { (num) -> Bool in
-return num < 5
-}, numbers: [1,3,8,2,99,32])
-print(filteredList)
+// Refine closure further to contain closure logic within another function
+func greaterThanFive(value: Int) -> Bool {
+    return value > 5
+}
+
+func divisibleByFive(value: Int) -> Bool {
+    return value % 5 == 0
+}
+
+//let filteredList = filterWithClosure(closure: greaterThanFive, numbers: [10,5,1,2,0,11,14,7])
+//print(filteredList)
+
+let filteredList = filterWithClosure(closure: divisibleByFive, numbers: [10,5,1,2,0,11,14,7])
+
+//let filteredList = filterWithClosure(closure: { (num) -> Bool in
+//return num < 20
+//}, numbers: [1,3,8,2,10,15,20,22,18,99,32])
+//print(filteredList)
+
